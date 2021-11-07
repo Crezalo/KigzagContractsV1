f555dfe2dc81887ec4581f26cabb1ca8177929ee
Ronnie Gandhi <ronniegandhi19999@gmail.com>
2021-11-07 16:38:36 +0530

    SPD-License-Identifier corrections

--- contracts/CreatorToken.sol
+++ contracts/CreatorToken.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT OR Apache-2.0
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
--- contracts/interfaces/IERC20X.sol
+++ contracts/interfaces/IERC20X.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT OR Apache-2.0
+// SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.4;
 
 interface IERC20X {
