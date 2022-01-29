1eeaa462c4463fa2db318cbc38766a8a2ba578ea
Ronnie Gandhi <ronniegandhi19999@gmail.com>
2022-01-29 13:24:40 +0530

    adding token template

--- contracts/LoyaltyTokens/USDC.sol
+++ contracts/LoyaltyTokens/USDC.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract WETH is ERC20{
+contract USDC is ERC20{
     string private _name;
     string private _symbol;
     uint256 private _totalSupply = 1000000000 * 10 ** 18;
